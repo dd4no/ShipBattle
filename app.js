6104f6c0c082a74a9a93b4550c8de3a633b3d91a
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-07-19 22:47:45 -0400

    Define grid edges

--- app.js
+++ app.js
@@ -18,18 +18,17 @@ document.addEventListener('DOMContentLoaded', () => {
     // Interface items
     const startButton = document.querySelector('#start');
     const rotateButton = document.querySelector('#rotate');
-    let ifHorizontal = true;
     const turnDisplay = document.querySelector('#turn');
     const infoDisplay = document.querySelector('#info');
     // Grid square arrays
     const playerSquares = [];
     const opponentSquares = [];
-
-    // Create Board
+    
+    // Create Board    
     // Make a grid
     function makeGrid(grid, squares) {
-        // 10 x 10 grid with 100 total squares
-        for(let i=0; i<100; i++){
+        // Create 100 total squares
+        for(let i=1; i<=100; i++){
             // Create a square
             const square = document.createElement('div');
             // Assign square an ID number
@@ -40,15 +39,40 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(square);
         }
     }
+    // Define edges
+    function addEdges(squares) {
+        // Top
+        for(let t=0; t<10; t++) {
+            squares[t].classList.add('top')
+        };
+        // Right
+        for(let r=9; r<100; r+=10) {
+            squares[r].classList.add('right')
+        };
+        // Bottom
+        for(let b=90; b<100; b++) {
+            squares[b].classList.add('bottom')
+        };
+        // Left
+        for(let l=0; l<=90; l+=10) {
+            squares[l].classList.add('left')
+        };
+    }
+    
     // Create player grid
     makeGrid(playerGrid, playerSquares);
+    addEdges(playerSquares);
+
     // Create opponent grid
     makeGrid(opponentGrid, opponentSquares);
-
+    addEdges(opponentSquares)
+    
+    
     // Ship Objects
     const fleet = [
         {
             name: 'destroyer',
+            size: 2,
             directions: [
                 [0,1],
                 [0,10]
@@ -56,13 +80,15 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name: 'submarine',
+            size: 3,
             directions: [
-                [0,1,2],
-                [0,10,20]
+                [-2,-1,0,1,2],
+                [-20,-10,0,10,20]
             ]
         },
         {
             name: 'cruiser',
+            size: 3,
             directions: [
                 [0,1,2],
                 [0,10,20]
@@ -70,6 +96,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name: 'battleship',
+            size: 4,
             directions: [
                 [0,1,2,3],
                 [0,10,20,30]
@@ -77,79 +104,77 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name: 'carrier',
+            size: 5,
             directions: [
                 [0,1,2,3,4],
                 [0,10,20,30,40]
             ]
         }
     ]
-    // console.log(fleet);
     
     // Deploy Random Opponent Fleet
     function deploy(ship) {
         console.log(ship.name);
-
+        
         // Generate a random starting point
-        let randomStart = Math.floor(Math.random() * 100);
+        let randomStart = Math.ceil(Math.random() * 100);
         console.log(`random start position: `  + randomStart);
         
         // Generate random ship orientation.
         let randomDirection = Math.floor(Math.random() * 2);
-        // console.log(`random direction: `  + randomDirection);
-
+        
+        // Assign increment value
         let directionIncrement;
+        let orientationArray = [];
         switch (randomDirection) {
             case 1:
                 // 1 = Vertical alignment. Increment direction by 10 squares
                 directionIncrement = 10;
+                orientationArray = ship.directions[1];
                 break;
                 
             default:
                 // 0 = Horizontal alignment. Increment direction by 1 square
                 directionIncrement = 1;
-                break;
+            orientationArray = ship.directions[0];
+            break;
         }
-        console.log("direction Increment: " + directionIncrement);
+            console.log("direction Increment: " + directionIncrement);
+            console.log("current orientation: " + orientationArray);
 
-        // Store current orientation array
-        // let current = ship.directions[randomDirection];
-        // console.log("current orientation: " + current);
-        
+                
+            // const isOccupied = orientationArray.some(index => opponentSquares[randomStart + index].classList.contains('occupied'));
+            // console.log(isOccupied);
+            
+            // const isRightEdge = current.some(index => (randomStart + index) % 10 === 10 - 1)
+            // console.log(isRightEdge);
+            
+            // const isLeftEdge = current.some(index => (randomStart + index) % 10 === 0)
+            // console.log(isLeftEdge);
+            
+            // if (!isOccupied && !isRightEdge && !isLeftEdge){
+                //     current.forEach(index => opponentSquares[randomStart + index].classList.add('occupied', ship.name))
+                // }
+                // else deploy(ship)
+            }
+            
+            // deploy(fleet[0])
+            deploy(fleet[1])
+            // deploy(fleet[2])
+            // deploy(fleet[3])
+            // deploy(fleet[4])
+           
+            
 
-        
 
-
-
-        // if (randomStart + ship.directions[0].length)
-        
-
-        // const isOccupied = current.some(index => opponentSquares[randomStart + index].classList.contains('occupied'));
-        // console.log(isOccupied);
-
-        // const isRightEdge = current.some(index => (randomStart + index) % 10 === 10 - 1)
-        // console.log(isRightEdge);
-
-        // const isLeftEdge = current.some(index => (randomStart + index) % 10 === 0)
-        // console.log(isLeftEdge);
-        
-        // if (!isOccupied && !isRightEdge && !isLeftEdge){
-        //     current.forEach(index => opponentSquares[randomStart + index].classList.add('occupied', ship.name))
-        // }
-        // else deploy(ship)
-    }
-
-    deploy(fleet[0])
-    // deploy(fleet[1])
-    // deploy(fleet[2])
-    // deploy(fleet[3])
-    // deploy(fleet[4])   
-
-    // function rotate(ship) {
-    //     if (isHorizontal) {
-    //         ship.classList.add('vertical');
-    //         isHorizontal = false;
-    //     }
-    // }
-
+            // let ifHorizontal = true;
+            // function rotate(ship) {
+                //     if (isHorizontal) {
+                    //         ship.classList.add('vertical');
+                    //         isHorizontal = false;
+                    //     }
+                    // }
+                    
 })
-
+                    
+                    
